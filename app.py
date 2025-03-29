3911e2015116dd96dc9719e83a9fd1183679fb0b
Yoonjae Cho <nicolasc0714@gmail.com>
2025-03-29 23:00:29 +0900

    Little Change

--- app.py
+++ app.py
@@ -12,10 +12,8 @@ import warnings
 warnings.filterwarnings("ignore")
 import sounddevice as sd
 import scipy.io.wavfile as wavfile
-import time
 import os
 import io
-import tempfile
 
 BASE_URL = "http://1.227.153.93:8000" # 배포용
 # BASE_URL = "http://192.168.55.18:8000" # 테스트용
